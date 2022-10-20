52d58c7c7daa46dd8bd09c7994b87b83af8cde42
YuriiYarmoliuk <116096922+YuriiYarmoliuk@users.noreply.github.com>
2022-10-20 20:58:30 +0300

    all arithmetic operations

---
+++ Main.java
@@ -0,0 +1,72 @@
+package com.cursor;
+
+public class Main {
+    public static void main(String[] args) {
+
+        byte a =  12 + 6;
+        byte a1 = 12 - 6;
+        byte a2 = 12 * 6;
+        byte a3 = 12 / 6;
+
+
+        short b = 9 + 9;
+        short b1 = 9 - 9;
+        short b2 = 9 * 9;
+        short b3 = 9 / 9;
+
+
+        long c = 45 + 9;
+        long c1 = 45 - 9;
+        long c2 = 45 * 9;
+        long c3 = 45 / 9;
+
+
+        double d = 24.5 + 7.7;
+        double d1 = 24.5 - 7.7;
+        double d2 = 24.5 * 7.7;
+        double d3 = 24.5 / 7.7;
+
+
+        float s = 15.3F + 4.15F;
+        float s1 = 15.3F - 4.15F;
+        float s2 = 15.3F * 4.15F;
+        float s3 = 15.3F / 4.15F;
+
+        System.out.println("byte сума = " + a);
+        System.out.println("byte різниця = " + a1);
+        System.out.println("byte добуток = " + a2);
+        System.out.println("byte частка = " + a3);
+
+        System.out.println( );
+
+        System.out.println("short сума = " + b);
+        System.out.println("short різниця = " + b1);
+        System.out.println("short добуток = " + b2);
+        System.out.println("short частка = " + b3);
+
+        System.out.println( );
+
+        System.out.println("long сума = " + c);
+        System.out.println("long різниця = " + c1);
+        System.out.println("long добуток = " + c2);
+        System.out.println("long частка = " + c3);
+
+        System.out.println( );
+
+        System.out.println("double сума = " + d);
+        System.out.println("double різниця = " + d1);
+        System.out.println("double добуток = " + d2);
+        System.out.println("double частка = " + d3);
+
+        System.out.println( );
+
+        System.out.println("float сума = " + s);
+        System.out.println("float різниця = " + s1);
+        System.out.println("float добуток = " + s2);
+        System.out.println("float частка = " + s3);
+
+
+
+
+    }
+}
